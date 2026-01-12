ae7ff61219856047d2a6e5b3c87badb92fd6c82a
TheYonaz <vannucci2@gmail.com>
2026-01-12 22:39:24 +0200

    fix: correct adminAuth middleware import for Pokemon TCG import route

--- server/cards/pokemonTCG/pokemonCardsRoutes.js
+++ server/cards/pokemonTCG/pokemonCardsRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../auth/authService");
-const adminAuth = require("../../middleware/adminAuth");
+const { verifyAdmin } = require("../../middleware/adminAuth");
 const {
   getCards,
   savePokemonDeck,
@@ -14,6 +14,6 @@ router.get("/pokemontcg", getCards);
 router.put("/pokemontcg/PdeckSave", auth, savePokemonDeck);
 router.get("/pokemontcg/pokemonDecks/:userId", auth, getPokemonDecks);
 router.delete("/pokemontcg/pokemonDecks/:DeckID", auth, deleteDeck);
-router.post("/pokemontcg/import", auth, adminAuth, importCards);
+router.post("/pokemontcg/import", auth, verifyAdmin, importCards);
 
 module.exports = router;
